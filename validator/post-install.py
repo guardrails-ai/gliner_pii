e37d48f65842cf886a7e9df6b9a809bc7a07742a
Aarav Navani <arav.navani@gmail.com>
2024-09-03 13:47:48 -0700

    post-install

--- validator/post-install.py
+++ validator/post-install.py
@@ -1,6 +1,9 @@
-print("post-install starting...")
-print(
-    "This is where you would do things like download nltk tokenizers or login to the HuggingFace hub..."
-)
-print("post-install complete!")
-# If you don't have anything to add here you should delete this file.
+import nltk
+import detoxify
+
+# Download NLTK data if not already present
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+print("NLTK stuff loaded successfully.")
